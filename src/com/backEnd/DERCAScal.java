fe99289560487dd3699d1f325ab1aca06fb57060
ErickSuy <er.cksuy@hotmail.com>
2016-05-13 19:01:34 -0600

    Inicializando proyecto

---
+++ src/com/backEnd/DERCAScal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.backEnd;
+
+import com.gui.*;
+
+/**
+ *
+ * @author erick
+ */
+public class DERCAScal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //new Principal().show();
+        new frameLogin().show();
+    }
+    
+}
